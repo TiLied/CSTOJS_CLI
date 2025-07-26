547a8500c2c0783c8440c1f00927d756a1b7d9fb
TiLied <TiLied@users.noreply.github.com>
2025-07-26 21:12:48 +0300

    Updated code for a newer CommandLine.

--- CSTOJS_CLI/Program.cs
+++ CSTOJS_CLI/Program.cs
@@ -2,316 +2,269 @@
 using System.Collections.Generic;
 using System;
 using System.CommandLine;
-using System.CommandLine.Binding;
 using System.IO;
 using System.Threading.Tasks;
-using System.Linq;
 
-namespace CSTOJS_CLI
+namespace CSTOJS_CLI;
+
+public class Program
 {
-	public class Program
+	private static int Main(string[] args)
 	{
-		private static async Task<int> Main(string[] args)
+		RootCommand rootCommand = new("CLI for CSharpToJavaScript library.");
+
+		//
+		//Options CSharpToJavaScript
+		Option<bool> debugOption = new("-Debug", "/Debug") 
 		{
-			RootCommand rootCommand = new("CLI for CSharpToJavaScript library.");
+			DefaultValueFactory = (e) => { return false; },
+			Description = "Debug. When set to true prints additional info to console, cs lines to js file."
+		};
 
-			Command fileCommand = new("file", "Choose a cs file or the folder with cs files.");
-			fileCommand.AddAlias("f");
-
-			Command continuousCommand = new("continuous", "Choose a cs file and generate continuously by watching the cs file.");
-			continuousCommand.AddAlias("c");
-
-
-			//Options CSharpToJavaScript
-			Option<bool> debugOption = new(
-				new string[] {
-					"-Debug",
-					"/Debug"},
-				"Debug. When set to true prints additional info to console, cs lines to js file.");
-			debugOption.SetDefaultValue(false);
-
-			fileCommand.AddOption(debugOption);
-			continuousCommand.AddOption(debugOption);
-
-			Option<bool> disableConsoleColorsOption = new(
-				new string[] {
-					"-DisableConsoleColors",
-					"/DisableConsoleColors"},
-				"Self-explanatory, Disable Console Colors.");
-			disableConsoleColorsOption.SetDefaultValue(false);
-
-			fileCommand.AddOption(disableConsoleColorsOption);
-			continuousCommand.AddOption(disableConsoleColorsOption);
-
-			Option<bool> disableConsoleOutputOption = new(
-				new string[] {
-					"-DisableConsoleOutput",
-					"/DisableConsoleOutput"},
-				"Self-explanatory, Disable Console Output.");
-			disableConsoleOutputOption.SetDefaultValue(false);
-
-			fileCommand.AddOption(disableConsoleOutputOption);
-			continuousCommand.AddOption(disableConsoleOutputOption);
-
-
-			Option<string> outPutPathOption = new(
-				new string[] {
-					"-OutPutPath",
-					"/OutPutPath"},
-				"Output path for javascript file.");
-			outPutPathOption.SetDefaultValue(Directory.GetCurrentDirectory());
-
-			fileCommand.AddOption(outPutPathOption);
-			continuousCommand.AddOption(outPutPathOption);
-
-
-			Option<bool> useVarOverLetOption = new(
-				new string[] {
-					"-UseVarOverLet",
-					"/UseVarOverLet"},
-				"Self-explanatory, Use var over let.");
-			useVarOverLetOption.SetDefaultValue(false);
-
-			fileCommand.AddOption(useVarOverLetOption);
-			continuousCommand.AddOption(useVarOverLetOption);
-
-
-			Option<bool> keepBraceOnTheSameLine = new(
-				new string[] {
-					"-KeepBraceOnTheSameLine",
-					"/KeepBraceOnTheSameLine"},
-				"Keep Brace '{' on the same line.");
-			keepBraceOnTheSameLine.SetDefaultValue(false);
-
-			fileCommand.AddOption(keepBraceOnTheSameLine);
-			continuousCommand.AddOption(keepBraceOnTheSameLine);
-
-
-			Option<bool> normalizeWhitespace = new(
-				new string[] {
-					"-NormalizeWhitespace",
-					"/NormalizeWhitespace"},
-				"Self-explanatory, Normalize Whitespace.");
-			normalizeWhitespace.SetDefaultValue(false);
-
-			fileCommand.AddOption(normalizeWhitespace);
-			continuousCommand.AddOption(normalizeWhitespace);
-
-			Option<bool> useStrictEquality = new(
-				new string[] {
-								"-UseStrictEquality",
-								"/UseStrictEquality"},
-				"Replace '==' with '===' and '!=' with '!=='.");
-			useStrictEquality.SetDefaultValue(false);
-
-			fileCommand.AddOption(useStrictEquality);
-			continuousCommand.AddOption(useStrictEquality);
-
-
-			Option<string> customCSTypesToJS = new(
-				new string[] {
-					"-CustomCSNamesToJS",
-					"/CustomCSNamesToJS"},
-				"List of custom names to convert. Example: Console-console,WriteLine-log");
-			customCSTypesToJS.SetDefaultValue(string.Empty);
-
-			fileCommand.AddOption(customCSTypesToJS);
-			continuousCommand.AddOption(customCSTypesToJS);
-
-
-			Argument<string> pathArgument = new("path", "Full path.");
-			fileCommand.AddArgument(pathArgument);
-			continuousCommand.AddArgument(pathArgument);
-
-			rootCommand.AddCommand(fileCommand);
-			rootCommand.AddCommand(continuousCommand);
-
-
-			CLICSTOJSOptionsBinder binder = new(debugOption,
-				disableConsoleColorsOption,
-				disableConsoleOutputOption,
-				outPutPathOption,
-				useVarOverLetOption,
-				keepBraceOnTheSameLine,
-				normalizeWhitespace,
-				useStrictEquality,
-				customCSTypesToJS
-				);
-
-
-			fileCommand.SetHandler(GenerateFile, pathArgument, binder);
-
-			continuousCommand.SetHandler(GenerateContinuously, pathArgument, binder);
-
-			return await rootCommand.InvokeAsync(args);
-		}
-
-		public static void GenerateContinuously(string? file, CLICSTOJSOptions options) 
+		Option<bool> disableConsoleColorsOption = new("-DisableConsoleColors", "/DisableConsoleColors") 
 		{
-			if (file == null)
-				return;
+			DefaultValueFactory = (e) => { return false; },
+			Description = "Self-explanatory, Disable Console Colors."
+		};
 
-			CSTOJS _CSTOJS = InitiateCSTOJS(options);
-
-			_CSTOJS.GenerateOneContinuously(file);
-
-			Console.WriteLine("Press any button to stop wathing.");
-			Console.ReadLine();
-
-			_CSTOJS.StopWatching();
-		}
-
-		public static async Task GenerateFile(string? file, CLICSTOJSOptions options)
+		Option<bool> disableConsoleOutputOption = new("-DisableConsoleOutput", "/DisableConsoleOutput")
 		{
-			if (file == null)
-				return;
+			DefaultValueFactory = (e) => { return false; },
+			Description = "Self-explanatory, Disable Console Output."
+		};
 
-			CSTOJS _CSTOJS = InitiateCSTOJS(options);
-
-			await _CSTOJS.GenerateOneAsync(file);
-		}
-
-		private static CSTOJS InitiateCSTOJS(CLICSTOJSOptions options)
+		Option<string> outPutPathOption = new("-OutPutPath", "/OutPutPath")
 		{
-			CSTOJS? _CSTOJS;
+			DefaultValueFactory = (e) => { return Directory.GetCurrentDirectory(); },
+			Description = "Output path for javascript file."
+		};
 
-			if (!options.IsDefault())
+		Option<bool> useVarOverLetOption = new("-UseVarOverLet", "/UseVarOverLet")
+		{
+			DefaultValueFactory = (e) => { return false; },
+			Description = "Self-explanatory, Use var over let."
+		};
+
+		Option<bool> keepBraceOnTheSameLine = new("-KeepBraceOnTheSameLine", "/KeepBraceOnTheSameLine") 
+		{
+			DefaultValueFactory = (e) => { return false; },
+			Description = "Keep Brace '{' on the same line."
+		};
+
+		Option<bool> normalizeWhitespace = new("-NormalizeWhitespace", "/NormalizeWhitespace") 
+		{
+			DefaultValueFactory = (e) => { return false; },
+			Description = "Self-explanatory, Normalize Whitespace."
+		};
+
+		Option<bool> useStrictEquality = new("-UseStrictEquality", "/UseStrictEquality") 
+		{
+			DefaultValueFactory =(e) => { return false; },
+			Description = "Replace '==' with '===' and '!=' with '!=='."
+		};
+
+		Option<string> customCSNamesToJS = new("-CustomCSNamesToJS", "/CustomCSNamesToJS") 
+		{
+			DefaultValueFactory = (e) => { return string.Empty; },
+			Description = "List of custom names to convert. Example: Console-console,WriteLine-log"
+		};
+
+		Argument<string> pathArgument = new("path") 
+		{
+			Description = "Full path."
+		};
+
+		//
+		Command fileCommand = new("file", "Choose a cs file or the folder with cs files.");
+		fileCommand.Aliases.Add("f");
+
+		fileCommand.Arguments.Add(pathArgument);
+		
+		fileCommand.Options.Add(debugOption);
+		fileCommand.Options.Add(disableConsoleColorsOption);
+		fileCommand.Options.Add(disableConsoleOutputOption);
+		fileCommand.Options.Add(outPutPathOption);
+		fileCommand.Options.Add(useVarOverLetOption);
+		fileCommand.Options.Add(keepBraceOnTheSameLine);
+		fileCommand.Options.Add(normalizeWhitespace);
+		fileCommand.Options.Add(useStrictEquality);
+		fileCommand.Options.Add(customCSNamesToJS);
+		//
+
+		//
+		Command continuousCommand = new("continuous", "Choose a cs file and generate it continuously by watching the cs file.");
+		continuousCommand.Aliases.Add("c");
+
+		continuousCommand.Arguments.Add(pathArgument);
+
+		continuousCommand.Options.Add(debugOption);
+		continuousCommand.Options.Add(disableConsoleColorsOption);
+		continuousCommand.Options.Add(disableConsoleOutputOption);
+		continuousCommand.Options.Add(outPutPathOption);
+		continuousCommand.Options.Add(useVarOverLetOption);
+		continuousCommand.Options.Add(keepBraceOnTheSameLine);
+		continuousCommand.Options.Add(normalizeWhitespace);
+		continuousCommand.Options.Add(useStrictEquality);
+		continuousCommand.Options.Add(customCSNamesToJS);
+		//
+
+
+		rootCommand.Subcommands.Add(fileCommand);
+		rootCommand.Subcommands.Add(continuousCommand);
+
+		fileCommand.SetAction(GenerateFile);
+		
+		continuousCommand.SetAction(GenerateContinuously);
+
+		ParseResult parseResult = rootCommand.Parse(args);
+		return parseResult.Invoke();
+}
+
+	public static void GenerateContinuously(ParseResult parseResult)
+	{
+		string? file = parseResult.GetValue<string?>("path");
+
+		if (file == null)
+			return;
+
+		CLICSTOJSOptions options = new()
+		{
+			Debug = parseResult.GetValue<bool>("-Debug"),
+			DisableConsoleColors = parseResult.GetValue<bool>("-DisableConsoleColors"),
+			DisableConsoleOutput = parseResult.GetValue<bool>("-DisableConsoleOutput"),
+			OutPutPath = parseResult.GetValue<string>("-OutPutPath"),
+			UseVarOverLet = parseResult.GetValue<bool>("-UseVarOverLet"),
+			KeepBraceOnTheSameLine = parseResult.GetValue<bool>("-KeepBraceOnTheSameLine"),
+			NormalizeWhitespace = parseResult.GetValue<bool>("-NormalizeWhitespace"),
+			UseStrictEquality = parseResult.GetValue<bool>("-UseStrictEquality"),
+			CustomCSNamesToJS = parseResult.GetValue<string>("-CustomCSNamesToJS")
+		};
+
+		CSTOJS _CSTOJS = InitiateCSTOJS(options);
+
+		_CSTOJS.GenerateOneContinuously(file);
+
+		Console.WriteLine("Press any button to stop wathing.");
+		Console.ReadLine();
+
+		_CSTOJS.StopWatching();
+	}
+
+	public static async Task GenerateFile(ParseResult parseResult)
+	{
+		string? file = parseResult.GetValue<string?>("path");
+
+		if (file == null)
+			return;
+
+		CLICSTOJSOptions options = new() 
+		{
+			Debug  = parseResult.GetValue<bool>("-Debug"),
+			DisableConsoleColors = parseResult.GetValue<bool>("-DisableConsoleColors"),
+			DisableConsoleOutput = parseResult.GetValue<bool>("-DisableConsoleOutput"),
+			OutPutPath = parseResult.GetValue<string>("-OutPutPath"),
+			UseVarOverLet  = parseResult.GetValue<bool>("-UseVarOverLet"),
+			KeepBraceOnTheSameLine  = parseResult.GetValue<bool>("-KeepBraceOnTheSameLine"),
+			NormalizeWhitespace  = parseResult.GetValue<bool>("-NormalizeWhitespace"),
+			UseStrictEquality = parseResult.GetValue<bool>("-UseStrictEquality"),
+			CustomCSNamesToJS = parseResult.GetValue<string>("-CustomCSNamesToJS")
+		};
+
+		CSTOJS _CSTOJS = InitiateCSTOJS(options);
+
+		await _CSTOJS.GenerateOneAsync(file);
+	}
+
+	private static CSTOJS InitiateCSTOJS(CLICSTOJSOptions options)
+	{
+		CSTOJS? _CSTOJS;
+
+		if (!options.IsDefault())
+		{
+			string customNames = options.CustomCSNamesToJS.Replace(" ", "").Trim();
+
+			List<Tuple<string, string>> _cstojsList = new();
+
+			if (customNames != string.Empty)
 			{
-				string customNames = options.CustomCSTypesToJS.Replace(" ", "").Trim();
-
-				List<Tuple<string, string>> _cstojsList = new();
-
-				if (customNames != string.Empty)
+				string[] _localTuples = [];
+				if (customNames.Contains(','))
 				{
-					List<string> _localTuples = new();
-					if (customNames.Contains(','))
-					{
-						_localTuples = customNames.Split(',').ToList();
+					_localTuples = customNames.Split(',');
 
-						for (int i = 0; i < _localTuples.Count; i++)
-						{
-							string[] _local = _localTuples[i].Split('-');
-							_cstojsList.Add(new(_local[0], _local[1]));
-						}
-					}
-					else
+					for (int i = 0; i < _localTuples.Length; i++)
 					{
-						if (customNames.Contains('-'))
-						{
-							string[] _local = customNames.Split('-');
-							_cstojsList.Add(new(_local[0], _local[1]));
-						}
+						string[] _local = _localTuples[i].Split('-');
+						_cstojsList.Add(new(_local[0], _local[1]));
 					}
 				}
-
-
-
-				_CSTOJS = new(new()
+				else
 				{
-					Debug = options.Debug,
-
-					DisableConsoleColors = options.DisableConsoleColors,
-
-					OutPutPath = options.OutPutPath,
-
-					UseVarOverLet = options.UseVarOverLet,
-					KeepBraceOnTheSameLine = options.KeepBraceOnTheSameLine,
-					NormalizeWhitespace = options.NormalizeWhitespace,
-					UseStrictEquality = options.UseStrictEquality,
-
-					CustomCSNamesToJS = _cstojsList
-				});
-			}
-			else
-				_CSTOJS = new();
-
-			return _CSTOJS;
-		}
-	}
-
-	public class CLICSTOJSOptions
-	{
-		public bool Debug { get; set; } = false;
-		public bool DisableConsoleColors { get; set; } = false;
-		public bool DisableConsoleOutput { get; set; } = false;
-		public string OutPutPath { get; set; } = Directory.GetCurrentDirectory();
-		public bool UseVarOverLet { get; set; } = false;
-		public bool KeepBraceOnTheSameLine { get; set; } = false;
-		public bool NormalizeWhitespace { get; set; } = false;
-		public bool UseStrictEquality { get; set; } = false;
-
-		public string CustomCSTypesToJS { get; set; } = string.Empty;
-
-		public bool IsDefault()
-		{
-			if (Debug != false ||
-				DisableConsoleColors != false ||
-				DisableConsoleOutput != false ||
-				UseVarOverLet != false ||
-				KeepBraceOnTheSameLine != false ||
-				NormalizeWhitespace != false ||
-				UseStrictEquality != false)
-			{
-				return false;
+					if (customNames.Contains('-'))
+					{
+						string[] _local = customNames.Split('-');
+						_cstojsList.Add(new(_local[0], _local[1]));
+					}
+				}
 			}
 
-			if (OutPutPath != Directory.GetCurrentDirectory())
-				return false;
 
-			if (CustomCSTypesToJS != string.Empty)
-				return false;
 
-			return true;
-		}
-	}
-
-	public class CLICSTOJSOptionsBinder : BinderBase<CLICSTOJSOptions>
-	{
-		private readonly Option<bool> _DebugOption;
-		private readonly Option<bool> _DisableConsoleColorsOption;
-		private readonly Option<bool> _DisableConsoleOutputOption;
-		private readonly Option<string> _OutPutPathOptionOption;
-		private readonly Option<bool> _UseVarOverLetOption;
-		private readonly Option<bool> _KeepBraceOnTheSameLine;
-		private readonly Option<bool> _NormalizeWhitespace;
-		private readonly Option<bool> _UseStrictEquality;
-
-		private readonly Option<string> _CustomCSTypesToJS;
-		public CLICSTOJSOptionsBinder(Option<bool> debugOption,
-			Option<bool> disableConsoleColorsOption,
-			Option<bool> disableConsoleOutputOption,
-			Option<string> outPutPathOptionOption,
-			Option<bool> useVarOverLetOption,
-			Option<bool> keepBraceOnTheSameLine,
-			Option<bool> normalizeWhitespace,
-			Option<bool> useStrictEquality,
-			Option<string> customCSTypesToJS)
-		{
-			_DebugOption = debugOption;
-			_DisableConsoleColorsOption = disableConsoleColorsOption;
-			_DisableConsoleOutputOption = disableConsoleOutputOption;
-			_OutPutPathOptionOption = outPutPathOptionOption;
-			_UseVarOverLetOption = useVarOverLetOption;
-			_KeepBraceOnTheSameLine = keepBraceOnTheSameLine;
-			_NormalizeWhitespace = normalizeWhitespace;
-			_UseStrictEquality = useStrictEquality;
-			_CustomCSTypesToJS = customCSTypesToJS;
-		}
-
-		protected override CLICSTOJSOptions GetBoundValue(BindingContext bindingContext)
-		{ 
-			return new()
+			_CSTOJS = new(new()
 			{
-				Debug = bindingContext.ParseResult.GetValueForOption(_DebugOption),
-				DisableConsoleColors = bindingContext.ParseResult.GetValueForOption(_DisableConsoleColorsOption),
-				DisableConsoleOutput = bindingContext.ParseResult.GetValueForOption(_DisableConsoleOutputOption),
-				OutPutPath = bindingContext.ParseResult.GetValueForOption(_OutPutPathOptionOption),
-				UseVarOverLet = bindingContext.ParseResult.GetValueForOption(_UseVarOverLetOption),
-				KeepBraceOnTheSameLine = bindingContext.ParseResult.GetValueForOption(_KeepBraceOnTheSameLine),
-				NormalizeWhitespace = bindingContext.ParseResult.GetValueForOption(_NormalizeWhitespace),
-				UseStrictEquality = bindingContext.ParseResult.GetValueForOption(_UseStrictEquality),
-				CustomCSTypesToJS = bindingContext.ParseResult.GetValueForOption(_CustomCSTypesToJS)
-			};
+				Debug = options.Debug,
+
+				DisableConsoleColors = options.DisableConsoleColors,
+
+				OutPutPath = options.OutPutPath,
+
+				UseVarOverLet = options.UseVarOverLet,
+				KeepBraceOnTheSameLine = options.KeepBraceOnTheSameLine,
+				NormalizeWhitespace = options.NormalizeWhitespace,
+				UseStrictEquality = options.UseStrictEquality,
+
+				CustomCSNamesToJS = _cstojsList
+			});
 		}
+		else
+			_CSTOJS = new();
+
+		return _CSTOJS;
+	}
+}
+
+public class CLICSTOJSOptions
+{
+	public bool Debug { get; set; } = false;
+	public bool DisableConsoleColors { get; set; } = false;
+	public bool DisableConsoleOutput { get; set; } = false;
+	public string OutPutPath { get; set; } = Directory.GetCurrentDirectory();
+	public bool UseVarOverLet { get; set; } = false;
+	public bool KeepBraceOnTheSameLine { get; set; } = false;
+	public bool NormalizeWhitespace { get; set; } = false;
+	public bool UseStrictEquality { get; set; } = false;
+
+	public string CustomCSNamesToJS { get; set; } = string.Empty;
+
+	public bool IsDefault()
+	{
+		if (Debug != false ||
+			DisableConsoleColors != false ||
+			DisableConsoleOutput != false ||
+			UseVarOverLet != false ||
+			KeepBraceOnTheSameLine != false ||
+			NormalizeWhitespace != false ||
+			UseStrictEquality != false)
+		{
+			return false;
+		}
+
+		if (OutPutPath != Directory.GetCurrentDirectory())
+			return false;
+
+		if (CustomCSNamesToJS != string.Empty)
+			return false;
+
+		return true;
 	}
 }
