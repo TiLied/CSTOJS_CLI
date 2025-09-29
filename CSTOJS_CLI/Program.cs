bae4dd6ff6e1b1deb3321086edacc737280067a9
TiLied <TiLied@users.noreply.github.com>
2025-09-29 15:36:06 +0300

    Updated CSharpToJavaScript to 0.1.0.

--- CSTOJS_CLI/Program.cs
+++ CSTOJS_CLI/Program.cs
@@ -1,12 +1,12 @@
 ﻿using CSharpToJavaScript;
 using System.Collections.Generic;
-using System;
 using System.CommandLine;
 using System.IO;
 using System.Threading.Tasks;
 using System.Diagnostics;
 using System.Xml;
 using System.Text;
+using CSharpToJavaScript.Utils;
 
 namespace CSTOJS_CLI;
 
@@ -14,110 +14,7 @@ public class Program
 {
 	private static int Main(string[] args)
 	{
-		RootCommand rootCommand = new("CLI for CSharpToJavaScript library.");
-
-		//
-		//Options CSharpToJavaScript
-		Option<bool> debugOption = new("-Debug", "/Debug") 
-		{
-			DefaultValueFactory = (e) => { return false; },
-			Description = "Debug. When set to true prints additional info to console, cs lines to js file."
-		};
-
-		Option<bool> disableConsoleColorsOption = new("-DisableConsoleColors", "/DisableConsoleColors") 
-		{
-			DefaultValueFactory = (e) => { return false; },
-			Description = "Self-explanatory, Disable Console Colors."
-		};
-
-		Option<bool> disableConsoleOutputOption = new("-DisableConsoleOutput", "/DisableConsoleOutput")
-		{
-			DefaultValueFactory = (e) => { return false; },
-			Description = "Self-explanatory, Disable Console Output."
-		};
-
-		Option<string> outPutPathOption = new("-OutPutPath", "/OutPutPath")
-		{
-			DefaultValueFactory = (e) => { return Directory.GetCurrentDirectory(); },
-			Description = "Output path for javascript file."
-		};
-
-		Option<bool> useVarOverLetOption = new("-UseVarOverLet", "/UseVarOverLet")
-		{
-			DefaultValueFactory = (e) => { return false; },
-			Description = "Self-explanatory, Use var over let."
-		};
-
-		Option<bool> keepBraceOnTheSameLine = new("-KeepBraceOnTheSameLine", "/KeepBraceOnTheSameLine") 
-		{
-			DefaultValueFactory = (e) => { return false; },
-			Description = "Keep Brace '{' on the same line."
-		};
-
-		Option<bool> normalizeWhitespace = new("-NormalizeWhitespace", "/NormalizeWhitespace") 
-		{
-			DefaultValueFactory = (e) => { return false; },
-			Description = "Self-explanatory, Normalize Whitespace."
-		};
-
-		Option<bool> useStrictEquality = new("-UseStrictEquality", "/UseStrictEquality") 
-		{
-			DefaultValueFactory =(e) => { return false; },
-			Description = "Replace '==' with '===' and '!=' with '!=='."
-		};
-
-		Option<string> customCSNamesToJS = new("-CustomCSNamesToJS", "/CustomCSNamesToJS") 
-		{
-			DefaultValueFactory = (e) => { return string.Empty; },
-			Description = "List of custom names to convert. Example: Console-console,WriteLine-log"
-		};
-
-		Argument<string> pathArgument = new("path") 
-		{
-			Description = "Full path."
-		};
-
-		//
-		Command fileCommand = new("file", "Choose a cs file or the folder with cs files.");
-		fileCommand.Aliases.Add("f");
-
-		fileCommand.Arguments.Add(pathArgument);
-		
-		fileCommand.Options.Add(debugOption);
-		fileCommand.Options.Add(disableConsoleColorsOption);
-		fileCommand.Options.Add(disableConsoleOutputOption);
-		fileCommand.Options.Add(outPutPathOption);
-		fileCommand.Options.Add(useVarOverLetOption);
-		fileCommand.Options.Add(keepBraceOnTheSameLine);
-		fileCommand.Options.Add(normalizeWhitespace);
-		fileCommand.Options.Add(useStrictEquality);
-		fileCommand.Options.Add(customCSNamesToJS);
-		//
-
-		//
-		Command continuousCommand = new("continuous", "Choose a cs file and generate it continuously by watching the cs file.");
-		continuousCommand.Aliases.Add("c");
-
-		continuousCommand.Arguments.Add(pathArgument);
-
-		continuousCommand.Options.Add(debugOption);
-		continuousCommand.Options.Add(disableConsoleColorsOption);
-		continuousCommand.Options.Add(disableConsoleOutputOption);
-		continuousCommand.Options.Add(outPutPathOption);
-		continuousCommand.Options.Add(useVarOverLetOption);
-		continuousCommand.Options.Add(keepBraceOnTheSameLine);
-		continuousCommand.Options.Add(normalizeWhitespace);
-		continuousCommand.Options.Add(useStrictEquality);
-		continuousCommand.Options.Add(customCSNamesToJS);
-		//
-		
-		rootCommand.Subcommands.Add(fileCommand);
-		rootCommand.Subcommands.Add(continuousCommand);
-
-		fileCommand.SetAction(GenerateFile);
-		
-		continuousCommand.SetAction(GenerateContinuously);
-
+		RootCommand rootCommand = new("Dotnet tool/cli for a CSharpToJavaScript library.");
 
 		Command setupCommand = new("setup", "Setup cstojs project.");
 		Argument<string> outputArgument = new("folder")
@@ -135,13 +32,13 @@ public class Program
 		
 		ParseResult parseResult = rootCommand.Parse(args);
 		return parseResult.Invoke();
-}
+	}
 
 	public static void SetupAction(ParseResult result)
 	{
 		string folder = result.GetRequiredValue<string>("folder");
 
-		Console.WriteLine("Running: 'dotnet new console -f net10.0'");
+		Log.InfoLine("Running: 'dotnet new console -f net10.0'");
 		ProcessStartInfo startInfo = new()
 		{
 			FileName = "dotnet",
@@ -155,21 +52,21 @@ public class Program
 		proc.Start();
 		proc.WaitForExit();
 
-		Console.WriteLine("Adding CSharpToJavaScript package: 'dotnet add package CSharpToJavaScript'");
+		Log.InfoLine("Adding CSharpToJavaScript package: 'dotnet add package CSharpToJavaScript'");
 		startInfo.Arguments = "add package CSharpToJavaScript";
 		proc.Start();
 		proc.WaitForExit();
 
-		Console.WriteLine($"Creating an output folder: '{folder}'");
+		Log.InfoLine($"Creating an output folder: '{folder}'");
 		Directory.CreateDirectory(folder);
 
-		Console.WriteLine($"Creating 'cstojs_options.xml'");
+		Log.InfoLine($"Creating 'cstojs_options.xml'");
 		XmlDocument doc = new();
 
 		XmlElement root = doc.CreateElement(string.Empty, "ProjectOptions", string.Empty);
 
 		XmlElement output = doc.CreateElement("Output");
-		output.SetAttribute("Folder", "./" + folder);
+		output.SetAttribute("Folder", folder);
 		root.AppendChild(output);
 
 		XmlElement file = doc.CreateElement("File");
@@ -179,238 +76,235 @@ public class Program
 		doc.AppendChild(root);
 		doc.Save("cstojs_options.xml");
 
-		Console.WriteLine($"Setup ended! Try running 'cstojs-cli translate'");
+		Log.InfoLine($"Setup ended! Try running 'cstojs-cli translate'");
 	}
 
 	public static async Task TranslateAction(ParseResult result)
 	{
-		CSTOJS cstojs = new();
+		/* TODO?
+		Assembly assembly = Assembly.LoadFile("../../CSharpToJavaScript/bin/Debug/net10.0/CSharpToJavaScript.dll");
+		var cstojsClass = assembly.GetType("CSharpToJavaScript.CSTOJS");
+		var fileData = assembly.GetType("CSharpToJavaScript.FileData");
+		
+		dynamic fileDataInst = assembly.CreateInstance("CSharpToJavaScript.FileData", false,
+			BindingFlags.ExactBinding,
+			null, null, null, null);
+		fileDataInst.SourceStr = "Console.WriteLine();";
+
+		MethodInfo m = cstojsClass.GetMethod("Translate");
+		dynamic ret = m.Invoke(null, new dynamic[] { new dynamic[] { fileDataInst }, null });
+		Log.WriteLine($"SampleMethod returned {ret[0].TranslatedStr}.");
+		*/
 		
 		CSTOJSOptions defaultOptions = new();
-		string currentFile = string.Empty;
+
+		FileData? currentFile = null;
 
 		string outputPath = string.Empty;
 
-		Dictionary<string, CSTOJSOptions> files = new();
-		
+		List<FileData> files = new();
+
 		string pathCombined = string.Empty;
+
+		//Options:
+		string? Debug = null;
+		string? UseVarOverLet = null;
+		string? KeepBraceOnTheSameLine = null;
+		string? NormalizeWhitespace = null;
+		string? UseStrictEquality = null;
+		string? TranslateFile = null;
 		
+		string? CustomCSNamesToJS = null;
+		string? AddSBAtTheTop = null;
+		string? AddSBAtTheBottom = null;
+
 		using (XmlReader reader = XmlReader.Create("cstojs_options.xml"))
 		{
 			while (reader.Read())
 			{
-
 				switch (reader.NodeType)
 				{
 					case XmlNodeType.Element:
 						{
-							Console.WriteLine("Start Element {0}", reader.Name);
+							Log.WriteLine($"Start Element {reader.Name}");
 
 							if (reader.Name == "Output")
 							{
-								outputPath = reader.GetAttribute(0);
+								string? _output = reader.GetAttribute("Folder");
+								if (_output == null)
+								{
+									Log.ErrorLine("Folder attribute is null!");
+									return;
+								}
+								if (!Directory.Exists(_output))
+								{
+									Log.ErrorLine($"Directory does not exists: {_output}");
+									return;
+								}
+								outputPath = _output;
 								break;
 							}
 							if (reader.Name == "File")
 							{
-								currentFile = reader.GetAttribute("Source");
+								string? _source = reader.GetAttribute("Source");
+								if (_source == null)
+								{
+									Log.ErrorLine("Source attribute is null!");
+									return;
+								}
+								if (!File.Exists(_source))
+								{
+									Log.ErrorLine($"File does not exists: {_source}");
+									return;
+								}
+
+								currentFile = new()
+								{
+									PathID = _source.Replace(".cs", ".js"),
+									SourceStr = File.ReadAllText(_source)
+								};
+
 								if (reader.IsEmptyElement)
-									files.Add(currentFile, defaultOptions);
+									currentFile.OptionsForFile = defaultOptions;
 								else
-									files.Add(currentFile, new());
+									currentFile.OptionsForFile = new();
+
+								files.Add(currentFile);
 								break;
 							}
 							if (reader.Name == "Option")
 							{
-								string? _debug = reader.GetAttribute("Debug");
+								Debug = reader.GetAttribute("Debug");
+								UseVarOverLet = reader.GetAttribute("UseVarOverLet");
+								KeepBraceOnTheSameLine = reader.GetAttribute("KeepBraceOnTheSameLine");
+								NormalizeWhitespace = reader.GetAttribute("NormalizeWhitespace");
+								UseStrictEquality = reader.GetAttribute("UseStrictEquality");
+								TranslateFile = reader.GetAttribute("TranslateFile");
 
-								if (currentFile == string.Empty)
+								CustomCSNamesToJS = reader.GetAttribute("CustomCSNamesToJS");
+								AddSBAtTheTop = reader.GetAttribute("AddSBAtTheTop");
+								AddSBAtTheBottom = reader.GetAttribute("AddSBAtTheBottom");
+
+								if (Debug != null)
 								{
-									if (_debug != null)
-										defaultOptions.Debug = bool.Parse(_debug);
+									if (currentFile == null)
+										defaultOptions.Debug = bool.Parse(Debug);
+									else
+										currentFile.OptionsForFile.Debug = bool.Parse(Debug);
 								}
-								else
+								if (UseVarOverLet != null)
 								{
-									if (_debug != null)
-										files[currentFile].Debug = bool.Parse(_debug);
+									if (currentFile == null)
+										defaultOptions.UseVarOverLet = bool.Parse(UseVarOverLet);
+									else
+										currentFile.OptionsForFile.UseVarOverLet = bool.Parse(UseVarOverLet);
 								}
+								if (KeepBraceOnTheSameLine != null)
+								{
+									if (currentFile == null)
+										defaultOptions.KeepBraceOnTheSameLine = bool.Parse(KeepBraceOnTheSameLine);
+									else
+										currentFile.OptionsForFile.KeepBraceOnTheSameLine = bool.Parse(KeepBraceOnTheSameLine);
+								}
+								if (NormalizeWhitespace != null)
+								{
+									if (currentFile == null)
+										defaultOptions.NormalizeWhitespace = bool.Parse(NormalizeWhitespace);
+									else
+										currentFile.OptionsForFile.NormalizeWhitespace = bool.Parse(NormalizeWhitespace);
+								}
+								if (UseStrictEquality != null)
+								{
+									if (currentFile == null)
+										defaultOptions.UseStrictEquality = bool.Parse(UseStrictEquality);
+									else
+										currentFile.OptionsForFile.UseStrictEquality = bool.Parse(UseStrictEquality);
+								}
+								if (TranslateFile != null)
+								{
+									if (currentFile == null)
+										defaultOptions.TranslateFile = bool.Parse(TranslateFile);
+									else
+										currentFile.OptionsForFile.TranslateFile = bool.Parse(TranslateFile);
+								}
+
+								if (CustomCSNamesToJS != null)
+								{
+									Dictionary<string, string> _customCSNamesToJSList = new();
+									string[] _localTuples;
+									
+									if (CustomCSNamesToJS.Contains(','))
+									{
+										_localTuples = CustomCSNamesToJS.Split(",");
+
+										for (int i = 0; i < _localTuples.Length; i++)
+										{
+											string[] _local = _localTuples[i].Split("-");
+											_customCSNamesToJSList.Add(_local[0], _local[1]);
+										}
+									}
+									else
+									{
+										if (CustomCSNamesToJS.Contains('-'))
+										{
+											string[] _local = CustomCSNamesToJS.Split("-");
+											_customCSNamesToJSList.Add(_local[0], _local[1]);
+										}
+									}
+									
+									if (currentFile == null)
+										defaultOptions.CustomCSNamesToJS = _customCSNamesToJSList;
+									else
+										currentFile.OptionsForFile.CustomCSNamesToJS = _customCSNamesToJSList;
+								}
+								if (AddSBAtTheTop != null)
+								{
+									StringBuilder _sb = new();
+									_sb.Append(AddSBAtTheTop);
+									
+									if (currentFile == null)
+										defaultOptions.AddSBAtTheTop = _sb;
+									else
+										currentFile.OptionsForFile.AddSBAtTheTop = _sb;
+								}
+								if (AddSBAtTheBottom != null)
+								{
+									StringBuilder _sb = new();
+									_sb.Append(AddSBAtTheBottom);
+
+									if (currentFile == null)
+										defaultOptions.AddSBAtTheBottom = _sb;
+									else
+										currentFile.OptionsForFile.AddSBAtTheBottom = _sb;
+								}
+								
+								Log.ErrorLine($"Unknown option: value: {reader.GetAttribute(0)}");
 								break;
 							}
 							break;
 						}
 					case XmlNodeType.EndElement:
 						{
-							Console.WriteLine("End Element {0}", reader.Name);
+							//Log.WriteLine($"End Element {reader.Name}");
 							if (reader.Name == "File")
-								currentFile = string.Empty;
+								currentFile = null;
 							break;
 						}
 					default:
-						Console.WriteLine($"-Other node: {reader.NodeType} | Value: {reader.Value}");
+						//Log.WriteLine($"-Other node: {reader.NodeType} | Value: {reader.Value}");
 						break;
 				}
 			}
 		}
 
-		foreach (KeyValuePair<string, CSTOJSOptions> keyValue in files)
+		FileData[] translatedFiles = CSTOJS.Translate(files.ToArray());
+
+		for (int i = 0; i < translatedFiles.Length; i++)
 		{
-			List<StringBuilder> _sb = cstojs.GenerateOne(keyValue.Key, keyValue.Value);
-			pathCombined = Path.Combine(outputPath, keyValue.Key.Replace(".cs", ".js"));
-			await File.WriteAllTextAsync(pathCombined, _sb[0].ToString());
-			Console.WriteLine($"--- Path: {Path.GetFullPath(pathCombined)}");
+			pathCombined = Path.Combine(outputPath, translatedFiles[i].PathID);
+			await File.WriteAllTextAsync(pathCombined, translatedFiles[i].TranslatedStr);
 		}
-	}
-
-
-	public static void GenerateContinuously(ParseResult parseResult)
-	{
-		string? file = parseResult.GetValue<string?>("path");
-
-		if (file == null)
-			return;
-
-		CLICSTOJSOptions options = new()
-		{
-			Debug = parseResult.GetValue<bool>("-Debug"),
-			DisableConsoleColors = parseResult.GetValue<bool>("-DisableConsoleColors"),
-			DisableConsoleOutput = parseResult.GetValue<bool>("-DisableConsoleOutput"),
-			OutPutPath = parseResult.GetValue<string>("-OutPutPath"),
-			UseVarOverLet = parseResult.GetValue<bool>("-UseVarOverLet"),
-			KeepBraceOnTheSameLine = parseResult.GetValue<bool>("-KeepBraceOnTheSameLine"),
-			NormalizeWhitespace = parseResult.GetValue<bool>("-NormalizeWhitespace"),
-			UseStrictEquality = parseResult.GetValue<bool>("-UseStrictEquality"),
-			CustomCSNamesToJS = parseResult.GetValue<string>("-CustomCSNamesToJS")
-		};
-
-		CSTOJS _CSTOJS = new();
-
-		CSTOJSOptions cstojsOptions = InitiateCSTOJS(options);
-
-		_CSTOJS.GenerateOneContinuously(file, cstojsOptions);
-
-		Console.WriteLine("Press any button to stop wathing.");
-		Console.ReadLine();
-
-		_CSTOJS.StopWatching();
-	}
-
-	public static async Task GenerateFile(ParseResult parseResult)
-	{
-		string? file = parseResult.GetValue<string?>("path");
-
-		if (file == null)
-			return;
-
-		CLICSTOJSOptions options = new() 
-		{
-			Debug  = parseResult.GetValue<bool>("-Debug"),
-			DisableConsoleColors = parseResult.GetValue<bool>("-DisableConsoleColors"),
-			DisableConsoleOutput = parseResult.GetValue<bool>("-DisableConsoleOutput"),
-			OutPutPath = parseResult.GetValue<string>("-OutPutPath"),
-			UseVarOverLet  = parseResult.GetValue<bool>("-UseVarOverLet"),
-			KeepBraceOnTheSameLine  = parseResult.GetValue<bool>("-KeepBraceOnTheSameLine"),
-			NormalizeWhitespace  = parseResult.GetValue<bool>("-NormalizeWhitespace"),
-			UseStrictEquality = parseResult.GetValue<bool>("-UseStrictEquality"),
-			CustomCSNamesToJS = parseResult.GetValue<string>("-CustomCSNamesToJS")
-		};
-
-		CSTOJS _CSTOJS = new();
-
-		CSTOJSOptions cstojsOptions = InitiateCSTOJS(options);
-
-		await _CSTOJS.GenerateOneAsync(file, cstojsOptions);
-	}
-
-	private static CSTOJSOptions InitiateCSTOJS(CLICSTOJSOptions options)
-	{
-		CSTOJSOptions? _opt;
-
-		if (!options.IsDefault())
-		{
-			string customNames = options.CustomCSNamesToJS.Replace(" ", "").Trim();
-
-			Dictionary<string, string> _cstojsList = new();
-
-			if (customNames != string.Empty)
-			{
-				string[] _localTuples = [];
-				if (customNames.Contains(','))
-				{
-					_localTuples = customNames.Split(',');
-
-					for (int i = 0; i < _localTuples.Length; i++)
-					{
-						string[] _local = _localTuples[i].Split('-');
-						_cstojsList.Add(_local[0], _local[1]);
-					}
-				}
-				else
-				{
-					if (customNames.Contains('-'))
-					{
-						string[] _local = customNames.Split('-');
-						_cstojsList.Add(_local[0], _local[1]);
-					}
-				}
-			}
-
-
-
-			_opt = new()
-			{
-				Debug = options.Debug,
-
-				DisableConsoleColors = options.DisableConsoleColors,
-
-				OutputPath = options.OutPutPath,
-
-				UseVarOverLet = options.UseVarOverLet,
-				KeepBraceOnTheSameLine = options.KeepBraceOnTheSameLine,
-				NormalizeWhitespace = options.NormalizeWhitespace,
-				UseStrictEquality = options.UseStrictEquality,
-
-				CustomCSNamesToJS = _cstojsList
-			};
-		}
-		else
-			_opt = new();
-
-		return _opt;
-	}
-}
-
-public class CLICSTOJSOptions
-{
-	public bool Debug { get; set; } = false;
-	public bool DisableConsoleColors { get; set; } = false;
-	public bool DisableConsoleOutput { get; set; } = false;
-	public string OutPutPath { get; set; } = Directory.GetCurrentDirectory();
-	public bool UseVarOverLet { get; set; } = false;
-	public bool KeepBraceOnTheSameLine { get; set; } = false;
-	public bool NormalizeWhitespace { get; set; } = false;
-	public bool UseStrictEquality { get; set; } = false;
-
-	public string CustomCSNamesToJS { get; set; } = string.Empty;
-
-	public bool IsDefault()
-	{
-		if (Debug != false ||
-			DisableConsoleColors != false ||
-			DisableConsoleOutput != false ||
-			UseVarOverLet != false ||
-			KeepBraceOnTheSameLine != false ||
-			NormalizeWhitespace != false ||
-			UseStrictEquality != false)
-		{
-			return false;
-		}
-
-		if (OutPutPath != Directory.GetCurrentDirectory())
-			return false;
-
-		if (CustomCSNamesToJS != string.Empty)
-			return false;
-
-		return true;
+		
+		Log.InfoLine($"--- Done: {Path.GetFullPath(outputPath)}");
 	}
 }
