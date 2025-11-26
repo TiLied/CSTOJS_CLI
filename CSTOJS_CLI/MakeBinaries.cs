e6d586755b2fdbdf5e5402db94c0b9bdfc258e41
TiLied <TiLied@users.noreply.github.com>
2025-11-26 19:38:49 +0300

    More logging...

--- CSTOJS_CLI/MakeBinaries.cs
+++ CSTOJS_CLI/MakeBinaries.cs
@@ -10,6 +10,7 @@ List<Tuple<string, string>> archs = new();
 archs.Add(new("any", "false"));
 
 //https://learn.microsoft.com/ru-ru/dotnet/core/rid-catalog
+
 archs.Add(new("win-x64", "true"));
 archs.Add(new("win-x86", "true"));
 archs.Add(new("win-arm64", "true"));
@@ -23,21 +24,26 @@ archs.Add(new("linux-bionic-arm64", "true"));
 
 archs.Add(new("osx-x64", "true"));
 archs.Add(new("osx-arm64", "true"));
+
 ProcessStartInfo startInfo = new()
 {
 	FileName = "dotnet",
 	UseShellExecute = false,
 	RedirectStandardOutput = true,
+	RedirectStandardError = true,
 	CreateNoWindow = true
 };
 for (int i = 0; i < archs.Count; i++)
 {
 	Console.WriteLine($"Running: 'dotnet build for: {archs[i].Item1}'");
-	startInfo.Arguments = $"build --configuration Release --output ./bin/binaries/CSTOJS_CLI-{archs[i].Item1}/ --runtime {archs[i].Item1} --self-contained {archs[i].Item2}";
+	startInfo.Arguments = $"build --configuration Release --property:OutputPath=./bin/binaries/CSTOJS_CLI-{archs[i].Item1}/ --runtime {archs[i].Item1} --self-contained {archs[i].Item2}";
 	Process proc = new() { StartInfo = startInfo };
 	proc.Start();
+	Console.WriteLine(proc.StandardOutput.ReadToEnd());
+	Console.WriteLine(proc.StandardError.ReadToEnd());
 	proc.WaitForExit();
 }
+
 if (!Path.Exists("./bin/zip/"))
 {
 	Directory.CreateDirectory("./bin/zip/");
@@ -45,7 +51,7 @@ if (!Path.Exists("./bin/zip/"))
 for (int i = 0; i < archs.Count; i++)
 {
 	string _directoryName = $"CSTOJS_CLI-{archs[i].Item1}";
-	
+
 	Console.WriteLine($"Zip binary: {_directoryName}");
 	ZipFile.CreateFromDirectory($"./bin/binaries/{_directoryName}/", $"./bin/zip/{_directoryName}.zip");
 }
